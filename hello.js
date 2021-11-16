d90cc77d66d364b8b87233b5d7706d02b55f5074
apgovinda <apgovinda@gmail.com>
2021-11-16 08:56:31 +0530

    2nd

--- hello.js
+++ hello.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
                  $.ajax({  
                      url: 'https://restapitest123.azurewebsites.net/api/hits', 
                      type: 'POST',  
+					 headers:{	"Access-Control-Allow-Origin":"https://htmlstaticazure.azurewebsites.net/"
+							 }
                      dataType: 'json',  
                      data:{ "browser": "chrome",   "device": "PC",  "time": "15-11-2021::13:00:00",  "data": "G test1" },  
                      success: function (data, textStatus, xhr) {  
